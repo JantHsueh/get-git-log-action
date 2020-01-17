22e9794eb23da0da299213a9a56745030aa6560b
xuexuan <384324069@qq.com>
2020-01-17 10:17:23 +0800

    默认使用最近的tag

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,16 @@
-#!/bin/sh
-echo "tag is $1"
-log=$(git log $1.. --pretty=format:'%an %cd %s \\n' --date=format:'%Y-%m-%d %H:%M:%S')
+#!/bin/bash
 
-echo ::set-output name=log::$log
+
+if [ $# != 0 ]
+then
+echo "tag is $1"
+time=$(git log $1.. --pretty=format:'%an, %cd %s \r' --date=format:'%Y-%m-%d %H:%M:%S')
+
+else
+tagName=$(git describe --abbrev=0 --tags)
+echo "tag is $tagName"
+time=$(git log $tagName.. --pretty=format:'%an, %cd %s \r' --date=format:'%Y-%m-%d %H:%M:%S')
+
+fi
+
+echo $time
