2456ffa887012f69ca95aa874650e4863f46f08d
xuexuan <384324069@qq.com>
2020-02-24 18:39:35 +0800

    \n -> \n (之前使用\n是可以换行)

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,12 +4,12 @@
 if [[ $1 != '' ]]
 then
 echo "tag is $1"
-log=$(git log $1.. --pretty=format:'- %cd %an\\n  %s\\n \\n' --date=format:'%Y-%m-%d %H:%M:%S')
+log=$(git log $1.. --pretty=format:'- %cd %an \n  %s\n \n' --date=format:'%Y-%m-%d %H:%M:%S')
 
 else
 tagName=$(git describe --abbrev=0 --tags)
 echo "tag is $tagName"
-log=$(git log $tagName.. --pretty=format:'- %cd %an\\n  %s\\n \\n' --date=format:'%Y-%m-%d %H:%M:%S')
+log=$(git log $tagName.. --pretty=format:'- %cd %an \n  %s\n \n' --date=format:'%Y-%m-%d %H:%M:%S')
 
 fi
 
